f5a8556d9490324df3391741de212747f757f45f
Sage Kearney <sagekearney@gmail.com>
2019-10-17 02:38:27 -0400

    added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,23 +3,20 @@
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = [];
 
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
+function checkForMatch(){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }}
 
-console.log("User flipped " + cardOne);
-
-cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-
-if (cardsInPlay.length === 2 ){
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-  } else{
-    alert("Sorry, try again.");
+function flipCard(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2 ){
+    checkForMatch();
   }
-} else{
-  console.log("we need another card");
-}
+};
+
+flipCard(0);
+flipCard(2);
