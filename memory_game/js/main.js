fecd8b404acc646d93ee4e87cb29ffcced044311
Sage Kearney <sagekearney@gmail.com>
2019-10-17 01:47:38 -0400

    created match alert

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user flipped " + cardOne);
+
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2 ){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  } else{
+    alert("Sorry, try again.");
+  }
+} else{
+  console.log("we need another card");
+}
