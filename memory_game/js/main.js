52862141b25ba3aaac5fd5adb8eb76a52835b3d2
Sage Kearney <sagekearney@gmail.com>
2019-10-17 12:02:32 -0400

    add flip card capability

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,17 +30,28 @@ function checkForMatch(){
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
-  }}
+  }};
 
-function flipCard(cardId){
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+function flipCard(){
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
   if (cardsInPlay.length === 2 ){
     checkForMatch();
   }
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+  for(var i=0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', cards[i]);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard()
